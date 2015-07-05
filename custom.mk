37a27c5535d02147e72cc5e15384405f039fc5ef
nikon <tonekit@hotmail.com>
2015-07-06 01:36:22 +0800

    删除custom.mk文件

--- custom.mk
+++
@@ -1,4 +0,0 @@
-BUILDDIR=/home/nikon/lcc42/lcc42-build/x86-ubuntu15-amd64
-LCCDIR=/home/nikon/lcc42/lcc42-build/x86-ubuntu15-amd64/
-TSTDIR=/home/nikon/lcc42/lcc42-build/x86-ubuntu15-amd64/tst
-HOSTFILE=etc/ubuntu15-amd64.c
