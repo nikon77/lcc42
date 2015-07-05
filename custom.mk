833fba80ab2cb06ae67f7e73dbca6523ef47c57f
nikon <tonekit@hotmail.com>
2015-07-06 02:14:35 +0800

    修复了编译脚本

---
+++ custom.mk
@@ -0,0 +1,4 @@
+BUILDDIR=/home/nikon/lcc42/lcc42-build/x86-ubuntu15-amd64
+LCCDIR=/home/nikon/lcc42/lcc42-build/x86-ubuntu15-amd64/
+TSTDIR=/home/nikon/lcc42/lcc42-build/x86-ubuntu15-amd64/tst
+HOSTFILE=etc/ubuntu15-amd64.c
